f068e6d2c2234f6433c205fd63dddac01fbad89b
Talles Souza <tallessouza117@gmail.com>
2025-06-05 19:58:45 -0300

    Erro na estrutura

--- Aula_14/Ex058.py
+++ Aula_14/Ex058.py
@@ -3,8 +3,8 @@
 from random import randint
 
 print('''Sou seu computador...
-      Acabei de pensar em um nímero entre 0 e 10.
-      Sará que você consegue advinhar qual foi?''')
+Acabei de pensar em um nímero entre 0 e 10.
+Sará que você consegue advinhar qual foi?''')
 n = randint(0, 10)
 p = int(input('Qual foi o seu palpite? '))
 t = 0
